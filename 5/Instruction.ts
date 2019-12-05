9ada6145e8500b40324ccb9d2eafb9a31ec01c61
Nathan Rabins <nrabins@gmail.com>
2019-12-05 07:37:04 -0600

    Day 5, part 1

--- 5/Instruction.ts
+++ 5/Instruction.ts
@@ -1,13 +1,31 @@
 export enum Operation {
-  Add,
-  Multiply,
-  Input,
-  Output
+  Add = 1,
+  Multiply = 2,
+  Input = 3,
+  Output = 4
+}
+
+function getNumberOfParameters(operation: Operation): number {
+  switch (operation) {
+    case Operation.Add:
+    case Operation.Multiply:
+      return 2;
+    case Operation.Input:
+    case Operation.Output:
+      return 1;
+    default:
+      throw `Unrecognized operation ${operation}`
+  }
 }
 
 export interface IParameter {
   parameterMode: ParameterMode;
   value: number;
+}
+
+export class Parameter implements IParameter {
+  constructor(public parameterMode: ParameterMode, public value: number) {
+  }
 }
 
 export enum ParameterMode {
@@ -21,15 +39,52 @@ export interface IInstruction {
 }
 
 export class Instruction implements IInstruction {
-  operation: Operation;  
+  operation: Operation;
   parameters: IParameter[];
 
   constructor(strip: number[], i: number) {
-    const opCode = strip[i];
-    this.operation = Operation.Add;
-    this.parameters = [];
-  } 
+    const instruction = strip[i];
+    const opCodeStr = getDigitFromRight(instruction, 1) + getDigitFromRight(instruction, 0)
+    let numberOfParameters;
+    switch (opCodeStr) {
+      case "01":
+        this.operation = Operation.Add;
+        numberOfParameters = 3;
+        break;
+      case "02":
+        this.operation = Operation.Multiply;
+        numberOfParameters = 3;
+        break;
+      case "03":
+        this.operation = Operation.Input;
+        numberOfParameters = 1;
+        break;
+      case "04":
+        this.operation = Operation.Output;
+        numberOfParameters = 1;
+        break;
+      default:
+        throw `Unrecognized opcode: ${opCodeStr}`;
+    }
 
+    if (i + numberOfParameters >= strip.length) {
+      throw "Ran out of strip"
+    }
+
+    const parameterValues = strip.slice(i+1, i+numberOfParameters+1);
+    this.parameters = parameterValues.map((value, index) => {
+      const parameterModeChar = getDigitFromRight(instruction, index+2);
+      let parameterMode: ParameterMode;
+      switch (parameterModeChar) {
+        case "0": parameterMode = ParameterMode.Position; break;
+        case "1": parameterMode = ParameterMode.Immediate; break;
+        default: throw `Unrecognized ParameterMode: ${parameterModeChar}`
+      }
+
+      return new Parameter(parameterMode, value);
+    });
+
+  }
 }
 
 /**
@@ -38,18 +93,18 @@ export class Instruction implements IInstruction {
  * @param positionFromRight The distance from right to get the digit (0 => 1's, 1=> 10's e.g.)
  */
 function getDigitFromRight(value: number, positionFromRight: number) {
-  const chars = value.toString().padStart(positionFromRight+1, "0").split("");
+  const chars = value.toString().padStart(positionFromRight + 1, "0").split("");
   return chars[chars.length - 1 - positionFromRight]
   // pfr = 3, chars = 3 [4] 2 1 5, length = 5, want chars[1] so... length-pfr-1
 }
 
-testGetDigitFromRight(12345, 0);
-testGetDigitFromRight(12345, 1);
-testGetDigitFromRight(12345, 2);
-testGetDigitFromRight(12345, 3);
-testGetDigitFromRight(12345, 4);
-testGetDigitFromRight(12345, 5);
-testGetDigitFromRight(12345, 100);
+// testGetDigitFromRight(12345, 0);
+// testGetDigitFromRight(12345, 1);
+// testGetDigitFromRight(12345, 2);
+// testGetDigitFromRight(12345, 3);
+// testGetDigitFromRight(12345, 4);
+// testGetDigitFromRight(12345, 5);
+// testGetDigitFromRight(12345, 100);
 
 function testGetDigitFromRight(value: number, positionFromRight: number) {
   console.log(`getDigitFromRight(${value}) = ${getDigitFromRight(value, positionFromRight)}`)
--- 5/part1.ts
+++ 5/part1.ts
@@ -1,7 +1,12 @@
 import * as fs  from 'fs'
+import { Computer } from './Computer';
 
 const text = fs.readFileSync('5/input.txt').toString('utf-8');
+// const text = '1002,4,3,4,33';
 
 const originalStrip = text.split(',').map((i: string) => parseInt(i, 10));
 
-const EXIT_CODE = 99;
+const computer = new Computer(originalStrip);
+const outputs = computer.run([1]);
+
+console.log(outputs);
