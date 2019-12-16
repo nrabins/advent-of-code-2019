54d5d466047a0870ca220de5d996a5cc517be145
Nate Rabins <nrabins@alkamitech.com>
2019-12-16 17:03:31 -0600

    Day 15, part 1

--- src/15/15.1.ts
+++ src/15/15.1.ts
@@ -1,8 +1,8 @@
 import Maze from './Maze';
 import * as fs from 'fs';
+import { Point } from '../util/Point';
 
 const mazeStr = fs.readFileSync("src/15/maze.txt").toString("utf-8");
-
 const maze = new Maze(mazeStr);
-
-debugger;
+const distance = maze.getDistanceBetween(maze.start as Point,  maze.oxygen as Point);
+console.log({ distance });
--- src/15/Maze.ts
+++ src/15/Maze.ts
@@ -7,8 +7,12 @@ enum Tile {
 
 class Cell {
   public distance?: number;
+  constructor(public point: Point, public tile: Tile) {
+  }
+}
 
-  constructor(public tile: Tile) {
+class QueueNode {
+  constructor(public point: Point, public distance: number) {
   }
 }
 
@@ -16,8 +20,8 @@ export default class Maze {
   // Cell[y][x]
   private grid: Cell[][] = [];
 
-  private start?: Point;
-  private oxygen?: Point;
+  public start?: Point;
+  public oxygen?: Point;
 
   constructor(file: string) {
     this.parse(file);
@@ -25,7 +29,7 @@ export default class Maze {
 
   private parse(file: string): void {
     const lines = file.split("\r\n");
-    
+
     lines.forEach((line, y) => {
       const row = [];
       for (let x = 0; x < line.length; x++) {
@@ -35,16 +39,15 @@ export default class Maze {
         } else if (symbol == "o") {
           this.oxygen = new Point(x, y);
         }
-        row.push(new Cell(Maze.parseTile(symbol)))
+        row.push(new Cell(new Point(x, y), Maze.parseTile(symbol)))
       }
       this.grid.push(row);
-    })
-    debugger;
+    });
   }
 
   private static parseTile(char: string): Tile {
     switch (char) {
-      case "█": 
+      case "█":
         return Tile.Wall;
       case ".":
       case "S":
@@ -54,4 +57,62 @@ export default class Maze {
         throw new Error(`Unrecognized tile: ${char}`);
     }
   }
+
+  private static rowNum: number[] = [-1, 0, 0, 1];
+  private static colNum: number[] = [0, -1, 1, 0];
+
+  public getDistanceBetween(start: Point, end: Point) {
+    const visited = Maze.initialize2dArray(this.grid, false);
+    visited[start.y][start.x] = true;
+
+    const q: QueueNode[] = [];
+
+    q.push(new QueueNode(new Point(start.x, start.y), 0));
+
+    while (q.length > 0) {
+      const curr = q[0];
+      const pt = curr.point;
+
+      if (pt.equals(end)) {
+        return curr.distance;
+      }
+
+      q.shift();
+
+      for (let i = 0; i < 4; i++) {
+        const row = pt.y + Maze.rowNum[i];
+        const col = pt.x + Maze.colNum[i];
+
+        if (this.isValid(row, col) &&
+          this.grid[row][col].tile == Tile.Open &&
+          !visited[row][col]) {
+            visited[row][col] = true;
+            q.push(new QueueNode(new Point(col, row), curr.distance + 1))
+        }
+      }
+    }
+
+    return -1;
+  }
+
+  private isValid(row: number, col: number): boolean {
+    const height = this.grid.length;
+    const width = this.grid[0].length;
+    return row >= 0 && row < width && col >= 0 && col < height;
+  }
+
+  private static initialize2dArray<T>(gridMap: { [y: number]: { [x: number]: any } }, initialValue: T | undefined = undefined): T[][] {
+    const arr: T[][] = [];
+    Object.keys(gridMap).map(yStr => parseInt(yStr, 10)).forEach(y => {
+      const row: T[] = [];
+      Object.keys(gridMap[y]).map(xStr => parseInt(xStr, 10)).forEach(x => {
+        if (initialValue) {
+          row.push(initialValue)
+        }
+      })
+      arr.push(row);
+    })
+    return arr;
+  }
+
 }
